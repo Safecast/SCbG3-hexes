022163b9c83cb6f53f7061c418d5248ec094e9dd
FakuFaku <fakufaku@gmail.com>
2013-01-08 17:35:45 +0900

    Reorganize folder.

--- run_me.sh
+++ run_me.sh
@@ -19,13 +19,13 @@ echo "Eject SD card, on the selector 1 is on and 2 off"
 read -p "ready ? [yY] "
 
 avrdude -p m32u4 -c $ISP_PROGRAMMER -P $ISP_TTYPORT -b $ISP_SPEED -U lfuse:w:0xde:m -U hfuse:w:0xd8:m -U efuse:w:0xcb:m
-avrdude -p m32u4 -c $ISP_PROGRAMMER -P $ISP_TTYPORT -b $ISP_SPEED -U flash:w:MassStorage-v3.0.5.hex
+avrdude -p m32u4 -c $ISP_PROGRAMMER -P $ISP_TTYPORT -b $ISP_SPEED -U flash:w:hex/MassStorage-v3.0.5.hex
 
 echo "SD card still ejected, on the selector 1 is off and 2 on"
 read -p "ready ? [yY] "
 
 avrdude -p m1284p -c $ISP_PROGRAMMER -P $ISP_TTYPORT -b $ISP_SPEED -U lfuse:w:0xff:m -U hfuse:w:0xdc:m -U efuse:w:0xfd:m
-avrdude -p m1284p -c $ISP_PROGRAMMER -P $ISP_TTYPORT -b $ISP_SPEED -U flash:w:optiboot_atmega1284p_8MHz.hex
+avrdude -p m1284p -c $ISP_PROGRAMMER -P $ISP_TTYPORT -b $ISP_SPEED -U flash:w:hex/optiboot_atmega1284p_8MHz.hex
 
 echo "Selector 1 is on, 2 is on."
 read -p "Done ? [yY] "
@@ -38,12 +38,12 @@ read -p "Done ? [yY] "
 
 read -p "Where is the AVR ISP programmer plugged ? [$SERIAL_TTYPORT] " SERIAL_TTYPORT
 
-avrdude -p m1284p -b $SERIAL_SPEED -c $SERIAL_PROGRAMMER -P $SERIAL_TTYPORT -U flash:w:bGeigie3-v3.0.5-iRover-JP.hex
+avrdude -p m1284p -b $SERIAL_SPEED -c $SERIAL_PROGRAMMER -P $SERIAL_TTYPORT -U flash:w:hex/bGeigie3-v3.0.5-iRover-JP.hex
 
 echo "Push SD card back in."
 read -p "Done ? [yY] "
 
 echo "Running test routine..."
 
-python bGeigie_QC.py -p $SERIAL_TTYPORT -b 57600
+python bin/bgeigie_diagnostic.py -p $SERIAL_TTYPORT -b 57600
 
