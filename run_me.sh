01ec56e92cb72e7c3ee29489b1731bc70355882b
Robin Scheibler <fakufaku@gmail.com>
2013-02-25 09:12:52 +0900

    Finally made it work on the real machine.

--- run_me.sh
+++ run_me.sh
@@ -20,7 +20,7 @@ echo "  * AVR programmer (USBTiny)."
 echo "  * USB-Serial dongle."
 
 #read -p "Click ENTER when ready..."
-xpdf -fullscreen images/Step1.pdf
+xpdf -fullscreen images/Step1.pdf 2> /dev/null
 
 echo "STEP 2: Make sure everything is ready:"
 echo "  * bGeigie device is connected to AVR ISP programmer."
@@ -28,7 +28,7 @@ echo "  * SD card ejected."
 echo "  * Selector 1 is ON and 2 is OFF."
 
 #read -p "Click ENTER when ready..."
-xpdf -fullscreen images/Step2.pdf
+xpdf -fullscreen images/Step2.pdf 2> /dev/null
 
 avrdude -p m32u4 -c $ISP_PROGRAMMER -U lfuse:w:0xde:m -U hfuse:w:0xd8:m -U efuse:w:0xcb:m
 avrdude -p m32u4 -c $ISP_PROGRAMMER -B 0.5 -U flash:w:hex/MassStorage-${MASSSTORAGE_VERSION}.hex
@@ -39,7 +39,7 @@ echo "  * SD card ejected."
 echo "  * Selector 1 is OFF and 2 is ON."
 
 #read -p "ready ? [yY] "
-xpdf -fullscreen images/Step3.pdf
+xpdf -fullscreen images/Step3.pdf 2> /dev/null
 
 avrdude -p m1284p -c $ISP_PROGRAMMER -U lfuse:w:0xff:m -U hfuse:w:0xdc:m -U efuse:w:0xfd:m
 avrdude -p m1284p -c $ISP_PROGRAMMER -B 0.5 -U flash:w:hex/optiboot_atmega1284p_8MHz.hex
@@ -50,14 +50,14 @@ echo "  * SD card ejected."
 echo "  * Selector 1 is OFF and 2 is ON."
 
 #read -p "ready ? [yY] "
-xpdf -fullscreen images/Step4.pdf
+xpdf -fullscreen images/Step4.pdf 2> /dev/null
 
-if [ ! -f $SERIAL_TTYPORT ];
+if [ ! -c ${SERIAL_TTYPORT} ];
 then
-  read -p "Where is the AVR ISP programmer plugged ? [$SERIAL_TTYPORT] " SERIAL_TTYPORT
+  read -p "Where is the AVR ISP programmer plugged ? " SERIAL_TTYPORT
 fi
 
-avrdude -p m1284p -b $SERIAL_SPEED -c $SERIAL_PROGRAMMER -P $SERIAL_TTYPORT -U flash:w:hex/bGeigie3-${BGEIGIE_VERSION}.hex
+avrdude -p m1284p -b $SERIAL_SPEED -c $SERIAL_PROGRAMMER -P ${SERIAL_TTYPORT} -U flash:w:hex/bGeigie3-${BGEIGIE_VERSION}.hex
 
 echo "Running test routine..."
 
