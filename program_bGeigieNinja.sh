f52f7a9329ef69f7486512387b0b7586fd5964ef
FakuFaku <fakufaku@gmail.com>
2013-03-27 19:53:39 +0900

    Corrected bug in Ninja programming script.

--- program_bGeigieNinja.sh
+++ program_bGeigieNinja.sh
@@ -4,8 +4,7 @@ SERIAL_PROGRAMMER=arduino
 SERIAL_TTYPORT=/dev/ttyUSB0
 SERIAL_SPEED=57600
 
-BGEIGIEMINI_VERSION=v1.4.5
-BGEIGIEMINI_VARIANT=JPVM
+BGEIGIENINJA_VERSION=v1.1.4
 
 failure(){
   xpdf -fullscreen images/Step5_failure.pdf 2> /dev/null
@@ -22,39 +21,8 @@ then
   read -p "Please type in device name (e.g. /dev/tty.usbserialA660JVIC): " SERIAL_TTYPORT
 fi
 
-echo "Please, choose firmware variant to upload:"
-echo "  1 - Post Office"
-echo "  2 - PlusShield, MTK GPS"
-echo "  3 - Standard, MTK GPS"
-echo "  4 - PlusShield, Canmore GPS"
-echo "  5 - Standard, Canmore GPS"
-read -p "Enter choice [1]: " CHOICE
-CHOICE=${CHOICE:-1}
-
-case $CHOICE in
-  1 )
-    BGEIGIEMINI_VARIANT=JPVM 
-    ;;
-  2 )
-    BGEIGIEMINI_VARIANT=PVM 
-    ;;
-  3 )
-    BGEIGIEMINI_VARIANT=M 
-    ;;
-  4 )
-    BGEIGIEMINI_VARIANT=PVC 
-    ;;
-  5 )
-    BGEIGIEMINI_VARIANT=C
-    ;;
-  * )
-    echo "Invalide choice. Please retry."
-    exit 1
-    ;;
-esac
-
 # program the device
-COMMAND="avrdude -p m328p -b ${SERIAL_SPEED} -c $SERIAL_PROGRAMMER -P ${SERIAL_TTYPORT} -U flash:w:hex/bGeigieMini-${BGEIGIEMINI_VERSION}-${BGEIGIEMINI_VARIANT}.hex"
+COMMAND="avrdude -p m328p -b ${SERIAL_SPEED} -c $SERIAL_PROGRAMMER -P ${SERIAL_TTYPORT} -U flash:w:hex/bGeigieNinja-${BGEIGIENINJA_VERSION}.hex"
 echo "Execute: ${COMMAND}"
 $COMMAND
 
