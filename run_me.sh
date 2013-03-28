de68bcf26124c4208b23858ba40d3c85cdd0acdc
FakuFaku <fakufaku@gmail.com>
2013-03-28 10:34:30 +0900

    Sync run_me.sh (bGeigie3 programming) with SafecastOffice branch. Added chip erase and lock.

--- run_me.sh
+++ run_me.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-ISP_PROGRAMMER="-c avrisp2 -P usb -B 0.5"
+ISP_PROGRAMMER="avrisp2"
+ISP_SPEED_SLOW=4
+ISP_SPEED_FAST=1
 
 SERIAL_PROGRAMMER=arduino
 SERIAL_TTYPORT=/dev/ttyUSB0
@@ -30,8 +32,21 @@ echo "  * Selector 1 is ON and 2 is OFF."
 #read -p "Click ENTER when ready..."
 xpdf -fullscreen images/Step2.pdf 2> /dev/null
 
+# perform chip erase
+COMMAND="avrdude -p m32u4 -c $ISP_PROGRAMMER -P usb  -B $ISP_SPEED_SLOW -e"
+echo $COMMAND
+$COMMAND
+if [ $? -ne 0 ];
+then
+  echo "Failure: couldn't erase 32U4."
+  failure
+  exit 1
+fi
+
 # program fuses of 32U4
-avrdude -p m32u4 $ISP_PROGRAMMER -U lfuse:w:0xde:m -U hfuse:w:0xd8:m -U efuse:w:0xcb:m
+COMMAND="avrdude -p m32u4 -c $ISP_PROGRAMMER -P usb  -B $ISP_SPEED_SLOW -U lfuse:w:0xde:m -U hfuse:w:0xd8:m -U efuse:w:0xcb:m"
+echo $COMMAND
+$COMMAND
 if [ $? -ne 0 ];
 then
   echo "Failure: couldn't program fuses of 32U4."
@@ -40,10 +55,23 @@ then
 fi
 
 # program firmware of 32U4
-avrdude -p m32u4 $ISP_PROGRAMMER -U flash:w:hex/MassStorage-${MASSSTORAGE_VERSION}.hex
+COMMAND="avrdude -p m32u4 -c $ISP_PROGRAMMER -P usb -B $ISP_SPEED_FAST -U flash:w:hex/MassStorage-${MASSSTORAGE_VERSION}.hex"
+echo $COMMAND
+$COMMAND
 if [ $? -ne 0 ];
 then
   echo "Failure: couldn't program firmware of 32U4."
+  failure
+  exit 1
+fi
+
+# lock the chip
+COMMAND="avrdude -p m32u4 -c $ISP_PROGRAMMER -P usb  -B $ISP_SPEED_SLOW -U lock:w:0x28:m"
+echo $COMMAND
+$COMMAND
+if [ $? -ne 0 ];
+then
+  echo "Failure: couldn't lock the 32U4."
   failure
   exit 1
 fi
@@ -56,7 +84,21 @@ echo "  * Selector 1 is OFF and 2 is ON."
 #read -p "ready ? [yY] "
 xpdf -fullscreen images/Step3.pdf 2> /dev/null
 
-avrdude -p m1284p $ISP_PROGRAMMER -U lfuse:w:0xff:m -U hfuse:w:0xdc:m -U efuse:w:0xfd:m
+# perorm a chip erase to unlock all lock bits before programming
+COMMAND="avrdude -p m1284p -c $ISP_PROGRAMMER -P usb -B $ISP_SPEED_SLOW -e"
+echo $COMMAND
+$COMMAND
+if [ $? -ne 0 ];
+then
+  echo "Failure: couldn't erase the chip."
+  failure
+  exit 1
+fi
+
+# now program the fuse
+COMMAND="avrdude -p m1284p -c $ISP_PROGRAMMER -P usb -B $ISP_SPEED_SLOW -U lfuse:w:0xff:m -U hfuse:w:0xdc:m -U efuse:w:0xfd:m"
+echo $COMMAND
+$COMMAND
 if [ $? -ne 0 ];
 then
   echo "Failure: couldn't program fuses of 1284P"
@@ -64,10 +106,24 @@ then
   exit 1
 fi
 
-avrdude -p m1284p $ISP_PROGRAMMER -U flash:w:hex/optiboot_atmega1284p_8MHz.hex
+# finally, upload the bootloader
+COMMAND="avrdude -p m1284p -c $ISP_PROGRAMMER -P usb -B $ISP_SPEED_FAST -U flash:w:hex/optiboot_atmega1284p_8MHz.hex"
+echo $COMMAND
+$COMMAND
 if [ $? -ne 0 ];
 then
   echo "Failure: couldn't program bootloader to 1284P"
+  failure
+  exit 1
+fi
+
+# now, lock the bootloader section and the fuses
+COMMAND="avrdude -p m1284p -c $ISP_PROGRAMMER -P usb -B $ISP_SPEED_SLOW -U lock:w:0x2c:m"
+echo $COMMAND
+$COMMAND
+if [ $? -ne 0 ];
+then
+  echo "Failure: couldn't lock the 1284p"
   failure
   exit 1
 fi
@@ -85,7 +141,9 @@ then
   read -p "Where is the AVR ISP programmer plugged ? " SERIAL_TTYPORT
 fi
 
-avrdude -p m1284p -b $SERIAL_SPEED -c $SERIAL_PROGRAMMER -P ${SERIAL_TTYPORT} -U flash:w:hex/bGeigie3-${BGEIGIE_VERSION}.hex
+COMMAND="avrdude -p m1284p -b $SERIAL_SPEED -c $SERIAL_PROGRAMMER -P ${SERIAL_TTYPORT} -U flash:w:hex/bGeigie3-${BGEIGIE_VERSION}.hex"
+echo $COMMAND
+$COMMAND
 if [ $? -ne 0 ];
 then
   echo "Failure: couldn't load bGeigie3 firmware to 1284P"
@@ -95,7 +153,9 @@ fi
 
 echo "Running test routine..."
 
-python bin/bgeigie_diagnostic.py -p $SERIAL_TTYPORT -b 57600
+COMMAND="python bin/bgeigie_diagnostic.py -p $SERIAL_TTYPORT -b 57600"
+echo $COMMAND
+$COMMAND
 
 if [ $? -eq 0 ];
 then
