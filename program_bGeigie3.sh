40c05b5729fc90d98628e77210f360417319f9c1
Robin Scheibler <fakufaku@gmail.com>
2014-01-03 16:32:06 +0100

    New upload script for revised bGeigie3 (no bootloader).

--- program_bGeigie3.sh
+++ program_bGeigie3.sh
@@ -9,21 +9,24 @@ SERIAL_PROGRAMMER=arduino
 SERIAL_TTYPORT=/dev/ttyUSB0
 SERIAL_SPEED=57600
 
-BGEIGIE_VERSION=v3.2.3
-MASSSTORAGE_VERSION=v3.2.3
+BGEIGIE_VERSION=v3.2.6
+MASSSTORAGE_VERSION=v3.2.6
+
+#XPDF_OPT=-fullscreen
 
 failure(){
-  xpdf -fullscreen images/Step5_failure.pdf 2> /dev/null
+  xpdf $XPDF_OPT images/Step_Failure.pdf 2> /dev/null
 }
+
 
 echo "Welcome to the flashing script for the bGeigie 3"
 echo "STEP 1: Make sure you have everything required:"
 echo "  * bGeigie device to program."
-echo "  * AVR programmer (USBTiny)."
-echo "  * USB-Serial dongle."
+echo "  * AVR programmer (AVR-ISP-MKII)."
+echo "  * USB A-male to mini-B cable."
 
 #read -p "Click ENTER when ready..."
-xpdf -fullscreen images/Step1.pdf 2> /dev/null
+xpdf $XPDF_OPT images/Step0.pdf 2> /dev/null
 
 echo "STEP 2: Make sure everything is ready:"
 echo "  * bGeigie device is connected to AVR ISP programmer."
@@ -31,7 +34,7 @@ echo "  * SD card ejected."
 echo "  * Selector 1 is ON and 2 is OFF."
 
 #read -p "Click ENTER when ready..."
-xpdf -fullscreen images/Step2.pdf 2> /dev/null
+xpdf $XPDF_OPT images/Step1.pdf 2> /dev/null
 
 # perform chip erase
 COMMAND="avrdude -p m32u4 -c $ISP_PROGRAMMER -P usb  -B $ISP_SPEED_SLOW -e"
@@ -83,7 +86,7 @@ echo "  * SD card ejected."
 echo "  * Selector 1 is OFF and 2 is ON."
 
 #read -p "ready ? [yY] "
-xpdf -fullscreen images/Step3.pdf 2> /dev/null
+xpdf $XPDF_OPT images/Step2.pdf 2> /dev/null
 
 # perorm a chip erase to unlock all lock bits before programming
 COMMAND="avrdude -p m1284p -c $ISP_PROGRAMMER -P usb -B $ISP_SPEED_SLOW -e"
@@ -129,22 +132,29 @@ then
   exit 1
 fi
 
+xpdf $XPDF_OPT images/Step3.pdf 2> /dev/null
+
 echo "STEP 4: Prepare bGeigie for operations."
 echo "  * Insert SD card."
 echo "  * Set selectors 1 and 2 to ON."
 echo "  * Press RESET button for 1 second."
 echo "  * Wait until LEDs finish blinking."
 
-#xpdf -fullscreen images/Step4.pdf 2> /dev/null
+xpdf $XPDF_OPT images/Step4.pdf 2> /dev/null
 
 echo "STEP 5: Test Mass Storage function."
 echo "  * Insert SD card."
-echo "  * Selector 1 is OFF and 2 is ON."
+echo "  * Selector 1 and 2 are ON."
 echo "  * Connect bGeigie to computer via USB mini cable."
+
+xpdf $XPDF_OPT images/Step5.pdf 2> /dev/null
+
+xpdf $XPDF_OPT images/Step6.pdf 2> /dev/null
+
 read -p "Is mass storage mounting correctly ? [nY] "
 echo "  * Unmount mass storage."
 
-#xpdf -fullscreen images/Step4.pdf 2> /dev/null
+xpdf $XPDF_OPT images/Step7.pdf 2> /dev/null
 
 echo "STEP 6: Test and configure bGeigie device."
 echo "  * Turn bGeigie ON by pressing push button for 2 seconds until LED blinks."
@@ -158,7 +168,7 @@ then
   exit 1
 fi
 
-#xpdf -fullscreen images/Step5.pdf 2> /dev/null
+#xpdf $XPDF_OPT images/Step5.pdf 2> /dev/null
 
 while [ ! -c ${SERIAL_TTYPORT} ];
 do
@@ -173,10 +183,10 @@ $COMMAND
 
 if [ $? -eq 0 ];
 then
-  xpdf -fullscreen images/Step5_success.pdf 2> /dev/null
+  xpdf $XPDF_OPT images/Step_Success.pdf 2> /dev/null
   echo "Success. You can close this terminal."
 else
-  xpdf -fullscreen images/Step5_failure.pdf 2> /dev/null
+  xpdf $XPDF_OPT images/Step_Failure.pdf 2> /dev/null
   echo "Failure. Try to find out what went wrong above."
 fi
 
